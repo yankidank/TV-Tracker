2e7f95b87b511d7848a32aeca816e9a2fcc8e127
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-04-29 12:41:05 -0700

    Removed unused diff prototype

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,15 +190,7 @@ function setDates(){ // Retrieves the schedule for the upcoming week
     combineSchedule = combineSchedule.concat(tempCombine)
   }
 }
-Array.prototype.diff = function(arr2) {
-  var ret = []
-  for(var i in this) {   
-    if(arr2.indexOf(this[i]) > -1){
-      ret.push(this[i])
-    }
-  }
-  return ret
-}
+
 function renderHome(){
   $("#tvColumn").empty()
   setDates() // Get the dates for the next week
